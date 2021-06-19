557cbc70caf4d7042b49e96f2283da899a66c685
Amer-Akmal <79959909+Amer-Akmal@users.noreply.github.com>
2021-06-19 13:09:46 +0800

    Add files via upload

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -23,31 +23,32 @@ export const listProducts = () =>  async (dispatch) => {
     }catch(error){
         dispatch({
             type:PRODUCT_LIST_FAIL,
-            payload:error.response && error.response.data.message
-            ? error.response.data.message
+            payload:error.response && error.response.data.detail
+            ? error.response.data.detail
             : error.message,
         })
     }
-}
+};
 
 
 export const listProductDetails = (id) =>  async (dispatch) => {
     try{
-        dispatch( { type: PRODUCT_DETAILS_REQUEST })
+        dispatch({ type: PRODUCT_DETAILS_REQUEST });
 
-        const { data } = await axios.get(`/api/products/${id}`)
+        const { data } = await axios.get(`/api/products/${id}`);
 
         dispatch({
-            type:PRODUCT_DETAILS_SUCCESS,
-            payload: data
-        })
+            type: PRODUCT_DETAILS_SUCCESS,
+            payload: data,
+        });
 
-    }catch(error){
+    } catch (error) {
         dispatch({
-            type:PRODUCT_DETAILS_FAIL,
-            payload:error.response && error.response.data.message
-            ? error.response.data.message
-            : error.message,
-        })
+            type: PRODUCT_DETAILS_FAIL,
+            payload:
+                error.response && error.response.data.detail
+                    ? error.response.data.detail
+                    : error.message,
+        });
     }
-}
+};
