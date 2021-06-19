8098ebf883baaebd66df910eefe425ff5fc0f856
Amer-Akmal <79959909+Amer-Akmal@users.noreply.github.com>
2021-06-19 13:24:28 +0800

    Add files via upload

---
+++ backend/base/urls/order_urls.py
@@ -0,0 +1,12 @@
+from atexit import register
+from django.urls import path
+from ..views import order_views as views
+
+
+urlpatterns = [
+    path('add/', views.addOrderItems, name='orders-add'),
+    path('myorders/', views.getMyOrders, name='myorders'),
+
+    path('<str:pk>/', views.getOrderById, name='user-order'),
+    path('<str:pk>/pay/', views.updateOrderToPaid, name='pay'),
+]
