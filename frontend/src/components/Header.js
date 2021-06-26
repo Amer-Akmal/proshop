1216aa4e129f9eba7ef73ee22e20d0eb3fb9ab45
Amer-Akmal <79959909+Amer-Akmal@users.noreply.github.com>
2021-06-26 17:47:30 +0800

    Add files via upload

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Navbar, Nav, Container, Row, NavDropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import SearchBox from './SearchBox'
 import { logout } from "../actions/userActions";
 
 function Header() {
@@ -21,8 +22,10 @@ function Header() {
           <LinkContainer to="/">
             <Navbar.Brand>ProShop</Navbar.Brand>
           </LinkContainer>
+
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
+            <SearchBox />
             <Nav className="mr-auto">
               <LinkContainer to="/cart">
                 <Nav.Link>
@@ -41,7 +44,7 @@ function Header() {
                   </NavDropdown.Item>
                 </NavDropdown>
               ) : (
-                <LinkContainer to="login">
+                <LinkContainer to="/login">
                   <Nav.Link>
                     <i className="fas fa-user"></i>Login
                   </Nav.Link>
